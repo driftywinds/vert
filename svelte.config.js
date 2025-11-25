18df6fd3650ba26635b2c5cf73bc2a000d15af70
drifty <github@drifty.win>
2025-11-25 12:56:29 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,17 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			relative: false,
-		},
-		env: {
-			publicPrefix: "PUB_",
-			privatePrefix: "PRI_",
-		},
-	},
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+  }
 };
 
 export default config;
